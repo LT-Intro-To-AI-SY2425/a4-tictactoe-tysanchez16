d7749f94439e9b33193a477b402766ad8beb428b
Tyler Sanchez <you@example.com>
2024-10-30 11:45:56 -0500

    notes

--- notes.py
+++ notes.py
@@ -2,3 +2,21 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n = "no name", a = 0):
+        self.name = n
+        self.age = a
+
+    def __str__(self):
+        s = f"{self.name} is {self.age} years old"
+        return s
+
+# instances of dog class
+logan = Dog("logan", 8)
+cookie = Dog("cookie", 8)
+maple = Dog("maple", 1)
+
+
+print(logan)
+print(cookie)
+print(maple)
