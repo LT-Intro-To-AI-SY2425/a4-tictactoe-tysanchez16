024c512e87263061f75f329f9d2a57661e99a674
Tyler Sanchez <you@example.com>
2024-10-31 11:31:40 -0500

    added notes

--- notes.py
+++ notes.py
@@ -3,13 +3,21 @@
 # (define-struct dog [fur_color name age favorite_food])
 
 class Dog:
+    species = "canis lupis familiariar"
     def __init__(self, n = "no name", a = 0):
         self.name = n
         self.age = a
+        self.fetch_count = 0
 
     def __str__(self):
         s = f"{self.name} is {self.age} years old"
         return s
+    
+    def play_fetch(self, num_fetch):
+        self.fetch_count += num_fetch
+    
+    def reset_fetch(self):
+        self.fetch_count = 0
 
 # instances of dog class
 logan = Dog("logan", 8)
@@ -17,6 +25,15 @@ cookie = Dog("cookie", 8)
 maple = Dog("maple", 1)
 
 
-print(logan)
+print(logan.fetch_count)
 print(cookie)
 print(maple)
+
+logan.play_fetch(4)
+print(logan.fetch_count)
+
+logan.play_fetch(8)
+print(logan.fetch_count)
+
+logan.reset_fetch()
+print(logan.fetch_count)
